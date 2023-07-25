16e608c22d22012f5c5ae44b3392399154da27f3
Hop Duong <hop281003@gmail.com>
2023-07-25 22:32:55 +0700

    asm_1_react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+function App() {
+    
+    return (
+        <>
+        <div className="w-96 mx-auto border border-gray-500 p-2">
+            <h1>LIST PRODUCT</h1>
+     
+        </div>
+      </>
+    );
+}
+
+export default App;
